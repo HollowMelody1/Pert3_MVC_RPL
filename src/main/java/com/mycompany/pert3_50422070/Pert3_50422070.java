b0084acd3243b2e6c5971f10b295e0ace5c1ad1b
HollowMelody1 <zaffar1703@gmail.com>
2025-11-30 16:53:45 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert3_50422070/Pert3_50422070.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422070;
+
+/**
+ *
+ * @author Zaffar
+ */
+public class Pert3_50422070 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
